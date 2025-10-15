0ddc11df6c0708c0c3b06993cd806bbb62c9e937
natia <natia.karamnishvili369@law.tsu.ge>
2025-10-16 01:22:55 +0400

    start decription with brute forcing from 1

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-
 ciphertext = 'Hvs Eiwqy Pfckb Tcl Xiadg Cjsf Hvs Zonm Rcu.'
+shift = 1
 
+def caesar_cipher_encript(text, shift):
+    result = ''
+
+    for i in range(len(text)):
+        char = text[i]
+
+        if char.isupper():
+            result += chr((ord(char) - shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+
+
+decripted = caesar_cipher_encript(ciphertext, shift)
+
+print('ciphered text is:', ciphertext)
+print('decripted etxt is: ', decripted)
